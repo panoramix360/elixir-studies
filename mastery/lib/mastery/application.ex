592beaa18d14ca2419f4e871ffaa09db751ccfcb
Lucas Reis <lucas.oreis@g.globo>
2021-06-03 16:21:58 -0300

    inserting mastery again

---
+++ mastery/lib/mastery/application.ex
@@ -0,0 +1,19 @@
+defmodule Mastery.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      { Mastery.Boundary.QuizManager,
+        [ name: Mastery.Boundary.QuizManager ] },
+      { Registry,
+        [ name: Mastery.Registry.QuizSession, keys: :unique ] },
+      { Mastery.Boundary.Proctor,
+        [ name: Mastery.Boundary.Proctor ] },
+      { DynamicSupervisor,
+        [ name: Mastery.Supervisor.QuizSession, strategy: :one_for_one ] },
+    ]
+
+    opts = [strategy: :one_for_one, name: Mastery.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ mastery/mix.exs
@@ -0,0 +1,30 @@
+defmodule Mastery.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mastery,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :eex],
+      mod: {Mastery.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:excoveralls, "~> 0.14.0"},
+      {:mastery_persistence, path: "./mastery_persistence"}
+    ]
+  end
+end
